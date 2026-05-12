c1f8d0daffe984c02addd9bfe565e87048935aa0
Danilo Porlles <danilopch7@gmail.com>
2026-05-12 16:58:25 -0500

    Actualizacion crear carpeta

--- MiniSO.bat
+++ MiniSO.bat
@@ -96,7 +96,14 @@ cls
 echo.
 set /p nuevo=Ruta y nombre de la nueva carpeta(ej. C:\videos\nombre_carpeta):
 md "%nuevo%"
-if %errorlevel%==0 (echo Carpeta nueva creada.) else (echo Error al crear la carpeta.)
+
+if %errorlevel%==0 (
+	echo Carpeta nueva creada.
+	echo.
+	echo La carpeta se creo en: %cd%
+	echo.
+)else (
+	echo Error al crear la carpeta.)
 echo.
 pause
 goto inicio
