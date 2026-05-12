f782afdb932f1fe38b8c79f9dd958defd1d50123
Danilo Porlles <danilopch7@gmail.com>
2026-05-12 14:58:27 -0500

    Agregando Condicional errorlevel

--- MiniSO.bat
+++ MiniSO.bat
@@ -1,11 +1,18 @@
 @echo off 
 :inicio 
 cls
-echo =============
-echo 	 Menu
-echo =============
+echo ==========================
+echo           Menu
+echo ==========================
+echo.
 echo 1. Copiar Archivo
 echo 2. Eliminar Archivo
+:: Algunas ideas: 
+::   - Renombrar archivo 
+::   - Crear un archivo y agregarle manualmente su contenido (copy con)
+::   - Copiar todos los archivos de una carpeta a otra (xcopy)
+::   - Crear, renombrar o eliminar carpetas
+::   - Mover archivos (move)
 echo 0. Salir
 ::Opciones restante por completar son 3
 echo.
@@ -34,11 +41,20 @@ goto inicio
 :eliminar
 cls
 echo.
-set /p elim=Ruta completa del archivo a eliminar (ej. C:\fotos\claseSO.jpg)   
+set /p elim=Ruta completa del archivo a eliminar (ej. C:\fotos\claseSO.jpg)  
 
 del "%elim%"
 echo.
-echo El archivo fue eliminado con exito
+
+:: Condicional en caso las rutas indicadas no existan o esten mal escritas
+
+if %errorlevel%==0 (
+	:: Caso exitoso
+    echo Archivo eliminado con exito. 
+) else (
+	:: Caso error
+    echo [ERROR] Verificar que la ruta ingresada exista y este correcta.
+)
 echo.
 
 pause
