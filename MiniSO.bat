bccb24a4bcb302615846f7f3b2c59230d25f0143
Alexis Orlando Gonzales Hernandez <alexis.gonzalescs@gmail.com>
2026-05-12 00:20:02 -0500

    MAQUETA INICIAL

--- MiniSO.bat
+++ MiniSO.bat
@@ -2,3 +2,26 @@
 :inicio 
 cls
 echo.
+echo ---Menu---
+echo 1. Copiar Archivo
+echo 2. Eliminar Archivo
+echo 0. Salir
+::Opciones restante por completar son 3
+echo.
+
+set /p opcion="Opcion: "
+if "%opcion%"=="1" goto copiar
+if "%opcion%"=="2" goto eliminar
+if "%opcion%"=="0" goto salir
+
+:copiar
+::ingresar scripts para copiar
+
+:eliminar
+::ingresar scripts para eliminar
+
+:salir
+echo.
+echo Saliendo del programa
+pause
+exit
